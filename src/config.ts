36df8cd35f1bbc33ea30f13aa5f051df51e89600
calvingomes <calvingomes045@gmail.com>
2025-11-27 00:49:05 +0530

    package / config updates

---
+++ src/config.ts
@@ -0,0 +1,48 @@
+export const email = "calvingomes045@gmail.com";
+
+export type SocialMediaItem = {
+  name: string;
+  url: string;
+};
+
+export const socialMedia: SocialMediaItem[] = [
+  { name: "GitHub", url: "" },
+  { name: "Instagram", url: "" },
+  { name: "Twitter", url: "" },
+  { name: "Linkedin", url: "" },
+  { name: "Codepen", url: "" },
+];
+
+export type NavLink = {
+  name: string;
+  url: string;
+};
+
+export const navLinks: NavLink[] = [
+  { name: "About", url: "/#about" },
+  { name: "Experience", url: "/#jobs" },
+  { name: "Work", url: "/#projects" },
+  { name: "Contact", url: "/#contact" },
+];
+
+export const colors = {
+  green: "#64ffda",
+  navy: "#0a192f",
+  darkNavy: "#020c1b",
+} as const;
+
+export const srConfig = (delay = 200, viewFactor = 0.25) => ({
+  origin: "bottom",
+  distance: "20px",
+  duration: 500,
+  delay,
+  rotate: { x: 0, y: 0, z: 0 },
+  opacity: 0,
+  scale: 1,
+  easing: "cubic-bezier(0.645, 0.045, 0.355, 1)",
+  mobile: true,
+  reset: false,
+  useDelay: "always",
+  viewFactor,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
