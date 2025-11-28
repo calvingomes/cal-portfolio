61331256e93b288bcc1edaa467a8891276f3d8c7
calvingomes <calvingomes045@gmail.com>
2025-11-29 02:23:10 +0530

    add jobs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Hero from "@/components/Hero/Hero";
 import About from "@/components/About/About";
+import Jobs from "@/components/Jobs/Jobs";
 
 export default function HomePage() {
   return (
     <main id="content">
       <Hero />
       <About />
+      <Jobs />
     </main>
   );
 }
