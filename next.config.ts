ae6cb788e4067273f6e813741da73883b980322c
calvingomes <calvingomes045@gmail.com>
2025-11-29 17:59:28 +0530

    removed styled-components

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  compiler: {
-    styledComponents: {
-      ssr: true,
-    }
-  },
+  compiler: {},
   turbopack: {},
 };
 
