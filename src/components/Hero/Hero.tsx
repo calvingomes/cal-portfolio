c1f1f92cccc9519f0211e5ab3b5b40fe4a3b943a
calvingomes <calvingomes045@gmail.com>
2025-11-29 05:52:01 +0530

    update hero content

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -31,6 +31,10 @@ const StyledHeroSection = styled.section`
     margin: 0;
   }
 
+  h3.big-heading {
+    color: var(--slate);
+  }
+
   p {
     margin: 20px 0 0;
     max-width: 540px;
@@ -66,7 +70,7 @@ const Hero = () => {
 
   if (!mounted) return <StyledHeroSection />;
 
-  const one = <h1 key="1">Hi, my name is</h1>;
+  const one = <h1 key="1">{`Hey there, I'm`}</h1>;
   const two = (
     <h2 key="2" className="big-heading">
       Calvin Gomes.
@@ -74,13 +78,15 @@ const Hero = () => {
   );
   const three = (
     <h3 key="3" className="big-heading">
-      I build things for the web.
+      I build great experiences for the web.
     </h3>
   );
   const four = (
     <p key="4">
-      I’m a software engineer specializing in building (and occasionally designing) exceptional
-      digital experiences. Currently, I’m focused on building accessible, human-centered products at{" "}
+      I love building fast, reliable, and accessible stuff for the web.
+      I focus on making sure complex ideas turn into smooth, user-friendly
+      digital experiences. Currently, {`I'm`} contributing to projects with the
+      excellent team at{" "}
       <a href="https://bolser.co.uk/" target="_blank" rel="noreferrer">
         Bolser Digital Agency
       </a>
