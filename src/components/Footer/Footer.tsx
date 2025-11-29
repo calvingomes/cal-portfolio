33e8064e24537f7ba0ea7b011dcded36e5c3bb2c
calvingomes <calvingomes045@gmail.com>
2025-11-29 17:34:03 +0530

    converted footer to module css

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,62 +1,14 @@
 "use client";
 
-import styled from "styled-components";
+import styles from "./Footer.module.css";
 import Icon from "@/components/icons/icon";
 import { socialMedia } from "@/config";
 
-const StyledFooter = styled.footer`
-  ${({ theme }) => theme.mixins.flexCenter};
-  flex-direction: column;
-  min-height: 70px;
-  padding: 15px;
-  text-align: center;
-`;
-
-const StyledSocialLinks = styled.div`
-  display: none;
-
-  @media (max-width: 768px) {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    color: var(--light-slate);
-  }
-
-  ul {
-    ${({ theme }) => theme.mixins.flexBetween};
-    list-style: none;
-    gap: 15px;
-    margin: 0;
-    padding: 0;
-
-    a {
-      padding: 10px;
-
-      svg {
-        width: 20px;
-        height: 20px;
-      }
-    }
-  }
-`;
-
-const StyledCredit = styled.div`
-  color: var(--light-slate);
-  font-family: var(--font-mono);
-  font-size: var(--fz-xxs);
-  line-height: 1;
-
-  a {
-    padding: 10px;
-  }
-`;
-
 const Footer = () => {
   return (
-    <StyledFooter>
-      {/* Mobile Only Social Icons */}
-      <StyledSocialLinks>
-        <ul>
+    <footer className={styles.footer}>
+      <div className={styles.socialLinks}>
+        <ul className={styles.ul}>
           {socialMedia.map(({ name, url }, i) => (
             <li key={i}>
               <a href={url} aria-label={name}>
@@ -65,14 +17,14 @@ const Footer = () => {
             </li>
           ))}
         </ul>
-      </StyledSocialLinks>
+      </div>
 
-      <StyledCredit tabIndex={-1}>
+      <div className={styles.credit} tabIndex={-1}>
         <a href="">
           <div>Designed and Built by Calvin with Next.js</div>
         </a>
-      </StyledCredit>
-    </StyledFooter>
+      </div>
+    </footer>
   );
 };
 
