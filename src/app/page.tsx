40219f3c15ad3b72605adc64a5219d656ee6ec91
calvingomes <calvingomes045@gmail.com>
2025-12-01 05:22:42 +0530

    add projects section inside featured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,15 @@ import Hero from "@/components/Hero/Hero";
 import About from "@/components/About/About";
 import Jobs from "@/components/Jobs/Jobs";
 import Featured from "@/components/Featured/Featured";
-import Projects from "@/components/Projects/Projects";
 import Contact from "@/components/Contact/Contact";
 
 export default function HomePage() {
   return (
-    <main>
+    <main id="content">
       <Hero />
       <About />
       <Jobs />
       <Featured />
-      <Projects />
       <Contact />
     </main>
   );
