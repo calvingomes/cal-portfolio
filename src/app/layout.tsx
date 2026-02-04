19a9e288f21b359f08abba5e969b3f4b6866bf3c
Calvin <34601104+calvingomes@users.noreply.github.com>
2026-02-04 22:29:37 +0400

    use Next third parties package for GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
@@ -19,36 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* GTM */}
+      <GoogleTagManager gtmId="GTM-KSQ37KL7" />
+
       <body className="antialiased" suppressHydrationWarning>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KSQ37KL7"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Navbar />
         {children}
         <Footer />
         {/* Vercel Analytics */}
         <Analytics />
-        {/* GTM */}
-        <Script
-          id="gtm"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];
-      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-      var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-      j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-      f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KSQ37KL7');
-    `,
-          }}
-        />
       </body>
     </html>
   );
