2eb5f6af5b752ee0716843bf28630240e5693496
Calvin <34601104+calvingomes@users.noreply.github.com>
2026-03-11 12:15:27 +0530

    update link to a for external

--- src/components/Navbar/NavShared.tsx
+++ src/components/Navbar/NavShared.tsx
@@ -6,44 +6,45 @@ import { navLinks } from "@/config";
 import navbarStyles from "./Navbar.module.css";
 
 export const NavLogo = () => (
-    <div className={navbarStyles.logo} tabIndex={-1}>
-        <Link href="/" aria-label="home">
-            <Image
-                src="/images/my-photo.webp"
-                alt="My Photo"
-                width={70}
-                height={70}
-                className={navbarStyles.logoImage}
-            />
-        </Link>
-    </div>
+  <div className={navbarStyles.logo} tabIndex={-1}>
+    <Link href="/" aria-label="home">
+      <Image
+        src="/images/my-photo.webp"
+        loading="lazy"
+        alt="My Photo"
+        width={70}
+        height={70}
+        className={navbarStyles.logoImage}
+      />
+    </Link>
+  </div>
 );
 
 export const NavListItems = ({ liClassName }: { liClassName: string }) => (
-    <>
-        {navLinks.map(({ url, name }, i) => (
-            <li key={i} className={liClassName}>
-                <Link href={url}>{name}</Link>
-            </li>
-        ))}
-    </>
+  <>
+    {navLinks.map(({ url, name }, i) => (
+      <li key={i} className={liClassName}>
+        <Link href={url}>{name}</Link>
+      </li>
+    ))}
+  </>
 );
 
 export const ResumeButton = ({
-    className,
-    onClick,
+  className,
+  onClick,
 }: {
-    className: string;
-    onClick: () => void;
+  className: string;
+  onClick: () => void;
 }) => (
-    <Link
-        className={className}
-        aria-label="resume"
-        href="/api/resume"
-        target="_blank"
-        rel="noopener noreferrer"
-        onClick={onClick}
-    >
-        RESUME
-    </Link>
+  <a
+    className={className}
+    aria-label="resume"
+    href="/api/resume"
+    target="_blank"
+    rel="noopener noreferrer"
+    onClick={onClick}
+  >
+    RESUME
+  </a>
 );
